f09383cf7fb038bb6f83f9409ea3f06699128873
christiaan.verwijs <christiaan.verwijs@christiaanverwijs.nl>
2013-12-02 21:34:44 +0100

    Disabled automatic migrations

--- Repositories/Migrations/Configuration.cs
+++ Repositories/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace ChristiaanVerwijs.MvcSiteWithEntityFramework.Repositories
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationsEnabled = false;
             AutomaticMigrationDataLossAllowed = true;
         }
 
