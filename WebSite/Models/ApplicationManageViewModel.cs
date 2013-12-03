e1e10aef50853f9f1b15c0ea934e8e9be22a5692
christiaan.verwijs <christiaan.verwijs@christiaanverwijs.nl>
2013-12-03 11:41:34 +0100

    Added missing files

---
+++ WebSite/Models/ApplicationManageViewModel.cs
@@ -0,0 +1,32 @@
+﻿using ChristiaanVerwijs.MvcSiteWithEntityFramework.Repositories;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChristiaanVerwijs.MvcSiteWithEntityFramework.WebSite.Models
+{
+    public class ApplicationManageViewModel : ApplicationViewModel
+    {
+        public ApplicationManageViewModel()
+            : base()
+        {
+        }
+        public ApplicationManageViewModel(Application application)
+            : base(application)
+        {
+        }
+
+        public Application ToDalEntity()
+        {
+            var model = new Application();
+            model.Id = this.Id;
+            model.Name = this.Name;
+            model.Description = this.Description;
+            model.TeamId = this.TeamId;
+            return model;
+        }
+    }
+}
